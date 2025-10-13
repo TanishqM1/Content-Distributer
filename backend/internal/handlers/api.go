21d06fc5355c54c2ee8a8b5feaa929124e09447c
TanishqM1 <113214169+TanishqM1@users.noreply.github.com>
2025-10-12 18:39:55 -0700

    setup handler to rerotue (need to implement logic function)

---
+++ backend/internal/handlers/api.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"github.com/go-chi/chi"
+	chimiddle "github.com/go-chi/chi/middleware"
+)
+
+// in this file, I need to setup the handler. While it typically uses middleware, and we re-route to that here, we don't have any middleware! there is no permissions-based
+// things that we need to handle here.
+
+func Handler(r *chi.Mux) {
+	// strip trailing slashes (from chi package)
+	r.Use(chimiddle.StripSlahes)
+
+	// setup route (no middleware from our end)
+	// Need to implement the PostContent function still!
+
+	r.Route("/post", func(router chi.Router) {
+		router.Get("/content", PostContent)
+	})
+}
